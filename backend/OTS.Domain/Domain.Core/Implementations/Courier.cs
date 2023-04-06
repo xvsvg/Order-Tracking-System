9ebf486aa3865bd87ccd207740ce8c085a255690
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-07 00:42:21 +0300

    feat: add implementations

---
+++ backend/OTS.Domain/Domain.Core/Implementations/Courier.cs
@@ -0,0 +1,12 @@
+﻿using OTS.Domain.Domain.Core.Contracts;
+using OTS.Domain.Domain.Core.ValueObjects;
+
+namespace OTS.Domain.Domain.Core.Implementations;
+
+public class Courier : Person
+{
+    protected Courier() { }
+
+    public Courier(FullName fullName, params ContactInfo[] contactInfo)
+        : base(fullName, contactInfo) { }
+}
