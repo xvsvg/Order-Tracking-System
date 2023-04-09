ed908a1963346762114f42b019ec23bd8f3289d3
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-09 14:09:48 +0300

    feat: add application DI

---
+++ backend/OTS.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace OTS.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection collection)
+    {
+        collection.AddMediatR(cfg =>
+            cfg.RegisterServicesFromAssemblies(typeof(DependencyInjection).Assembly));
+
+        collection.AddAutoMapper(typeof(DependencyInjection).Assembly);
+
+        return collection;
+    }
+}
