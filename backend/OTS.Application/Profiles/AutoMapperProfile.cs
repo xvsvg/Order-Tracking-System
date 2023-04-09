207e7a9781a8fa1cc9423fc11055d3194b31d114
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-09 16:54:55 +0300

    feat: add automapper config (might get rid of it)

---
+++ backend/OTS.Application/Profiles/AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using OTS.Application.Dto;
+using OTS.Domain.Domain.Core.Implementations;
+
+namespace OTS.Application.Profiles;
+
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        CreateMap<Courier, CourierDto>();
+        CreateMap<Customer, CourierDto>();
+        CreateMap<CourierDto, CourierDto>();
+        CreateMap<Order, OrderDto>();
+    }
+}
