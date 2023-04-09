3bd6f15099df6d40dcb11a13eec6fd0379aa36ff
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-09 13:56:13 +0300

    feat: add db context

---
+++ backend/OTS.Persistence/DatabaseContexts/OtsDatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OTS.Application.Contracts.Database;
+using OTS.Domain.Domain.Core.Contracts;
+using OTS.Domain.Domain.Core.Implementations;
+
+namespace OTS.Persistence.DatabaseContexts;
+
+public sealed class OtsDatabaseContext : DbContext, IOtsDatabaseContext
+{
+    public OtsDatabaseContext(DbContextOptions<OtsDatabaseContext> options)
+        : base(options) { }
+
+    public DbSet<Order> Orders { get; set; } = null!;
+    public DbSet<Person> People { get; set; } = null!;
+    public DbSet<Courier> Couriers { get; set; } = null!;
+    public DbSet<Customer> Customers { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(OtsDatabaseContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
