1d7188498c7808cefb01ffcdfe6b37f95401bab1
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-09 13:56:26 +0300

    feat: add entity configurations

---
+++ backend/OTS.Persistence/EntityTypeConfigurations/CustomerConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OTS.Domain.Domain.Core.Implementations;
+
+namespace OTS.Persistence.EntityTypeConfigurations;
+
+public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+{
+    public void Configure(EntityTypeBuilder<Customer> builder)
+    {
+        builder.HasOne(c => c.AccountCredentials)
+            .WithOne()
+            .HasForeignKey<Customer>(c => c.PersonId);
+
+        builder.Navigation(c => c.OrderHistory).HasField("_orderHistory");
+    }
+}
---
+++ backend/OTS.Persistence/EntityTypeConfigurations/OrderConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OTS.Domain.Domain.Core.Implementations;
+
+namespace OTS.Persistence.EntityTypeConfigurations;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.HasKey(x => x.OrderId);
+        builder.Property(x => x.DispatchDate);
+        builder.Property(x => x.DeliveryDate);
+
+        builder.HasOne(x => x.Courier)
+            .WithOne()
+            .HasForeignKey<Courier>(c => c.Customer!.PersonId);
+    }
+}
