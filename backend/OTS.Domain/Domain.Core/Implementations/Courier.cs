be4f32d10e865dfcbe46601cfe3ad6cfbdd9a4b2
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-09 14:00:18 +0300

    Revert "feat: add db information in appsetting.json"
    This reverts commit 98a3b3ac4004c2af5698598fae5d8d61112f9fbf.

--- backend/OTS.Domain/Domain.Core/Implementations/Courier.cs
+++ backend/OTS.Domain/Domain.Core/Implementations/Courier.cs
@@ -7,13 +7,6 @@ public class Courier : Person
 {
     protected Courier() { }
 
-    public Courier(Customer customer, FullName fullName, params ContactInfo[] contactInfo)
-        : base(fullName, contactInfo)
-    {
-        ArgumentNullException.ThrowIfNull(customer);
-
-        Customer = customer;
-    }
-
-    public Customer? Customer { get; set; }
+    public Courier(FullName fullName, params ContactInfo[] contactInfo)
+        : base(fullName, contactInfo) { }
 }
