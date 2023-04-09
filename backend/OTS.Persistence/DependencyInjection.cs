a3b2adeb87d4b113aa8994ee10eeb9199a2dcb79
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-09 13:56:44 +0300

    feat: add persistence DI

---
+++ backend/OTS.Persistence/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using OTS.Application.Contracts.Database;
+using OTS.Persistence.DatabaseContexts;
+
+namespace OTS.Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(
+        this IServiceCollection collection,
+        Action<DbContextOptionsBuilder> configuration)
+    {
+        collection.AddDbContext<IOtsDatabaseContext, OtsDatabaseContext>(configuration);
+
+        return collection;
+    }
+}
