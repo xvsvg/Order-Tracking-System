a6ccb6733c13d76df5be95b82043f699d5fada7f
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-09 14:10:10 +0300

    feat: modified entrypoint

--- backend/OTS.Presentation/Program.cs
+++ backend/OTS.Presentation/Program.cs
@@ -1,6 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using OTS.Application;
+using OTS.Persistence;
+using OTS.Persistence.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var configuration = new PostgresConfiguration();
+builder.Configuration.GetSection("PostgresConfiguration").Bind(configuration);
+
+builder.Services.AddPersistence(o =>
+    o.UseNpgsql(configuration.ToConnectionString("OTSDatabase"))
+        .UseLazyLoadingProxies());
+
+builder.Services.AddApplication();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(o =>
+{
+    o.AddPolicy("Loyal policy", policy =>
+    {
+        policy.AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowAnyOrigin();
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.Run();
