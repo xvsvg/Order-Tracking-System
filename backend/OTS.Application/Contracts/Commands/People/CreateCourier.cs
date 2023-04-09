8b9b410e80a0b676c855226d0559c1eb773b308a
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-09 16:54:01 +0300

    feat: add user commands contracts

---
+++ backend/OTS.Application/Contracts/Commands/People/CreateCourier.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using OTS.Application.Dto;
+
+namespace OTS.Application.Contracts.Commands.People;
+
+internal static class CreateCourier
+{
+    public record Command(Guid? CustomerId, string Fullname, params string[] ContactInfo ) : IRequest<Response>;
+
+    public record Response(CourierDto Courier);
+}
---
+++ backend/OTS.Application/Contracts/Commands/People/CreateCustomer.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using OTS.Application.Dto;
+
+namespace OTS.Application.Contracts.Commands.People;
+
+internal static class CreateCustomer
+{
+    public record Command
+        (string Fullname, string Email, string Password, params string[] ContactInfo) : IRequest<Response>;
+
+    public record Response(CustomerDto Customer);
+}
