60163c1c4ac5d9cc657de0e1097ec6ad4a0d5f57
xvsvg <pobedotriples@gmail.com>
2023-06-17 23:45:50 +0300

    feat: add value converters

---
+++ src/Infrastructure/Infrastructure.DataAccess/ValueConverters/ContactInfoValueConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using Domain.Core.ValueObjects;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Infrastructure.DataAccess.ValueConverters;
+
+public class ContactInfoValueConverter : ValueConverter<IEnumerable<ContactInfo>, string>
+{
+    public ContactInfoValueConverter()
+        : base(x => ConvertTo(x), x => ConvertFrom(x))
+    {
+    }
+
+    private static string ConvertTo(IEnumerable<ContactInfo> c)
+    {
+        return c.Select(x => x.Contact)
+            .Aggregate((current, next) => current + $"{next}")
+            .ToString()
+            .Trim();
+    }
+
+    private static IEnumerable<ContactInfo> ConvertFrom(string c)
+    {
+        return c.Split(' ')
+            .Select(name => new ContactInfo(name))
+            .ToList();
+    }
+}
---
+++ src/Infrastructure/Infrastructure.DataAccess/ValueConverters/FullnameValueConverter.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Core.ValueObjects;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Infrastructure.DataAccess.ValueConverters;
+
+public class FullnameValueConverter : ValueConverter<FullName, string>
+{
+    public FullnameValueConverter()
+        : base(x => $"{x.FirstName} {x.MiddleName} {x.LastName}",
+            x => new FullName(string.Empty, string.Empty, string.Empty))
+    {
+    }
+}
