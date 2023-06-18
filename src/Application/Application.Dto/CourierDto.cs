c9ecad25b00f94535f667033ad6127b4035e0310
xvsvg <pobedotriples@gmail.com>
2023-06-18 19:22:57 +0300

    feat: add dto

--- src/Application/Application.Dto/CourierDto.cs
+++ src/Application/Application.Dto/CourierDto.cs
@@ -1,17 +1,3 @@
 ﻿namespace Application.Dto;
 
-public class CourierDto
-{
-    public CourierDto(Guid id, string name, IEnumerable<string> contactInfo, Guid? customerId)
-    {
-        Id = id;
-        Name = name;
-        CustomerId = customerId;
-        ContactInfo = contactInfo.ToList();
-    }
-
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public Guid? CustomerId { get; set; }
-    public List<string> ContactInfo { get; set; }
-}
+public record CourierDto(Guid Id, string Name, IEnumerable<string> ContactInfo, IEnumerable<OrderDto> DeliveryList);
--- src/Application/Application.Dto/OrderDto.cs
+++ src/Application/Application.Dto/OrderDto.cs
@@ -1,13 +1,3 @@
 ﻿namespace Application.Dto;
 
-public class OrderDto
-{
-    public OrderDto(Guid id, CourierDto courier)
-    {
-        Id = id;
-        Courier = courier;
-    }
-
-    public Guid Id { get; set; }
-    public CourierDto Courier { get; set; }
-}
+public record OrderDto(Guid Id, CustomerDto Customer, DateTime DispatchDate, DateTime? DeliveryDate, CourierDto? Courier);
