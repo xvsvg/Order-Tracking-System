440c20d1ef7f5fed0c40a8c4fb97e92c227d187c
xvsvg <pobedotriples@gmail.com>
2023-06-22 14:10:04 +0300

    feat: add get order tests

---
+++ tests/ApplicationMicroservice/Test.Handlers/Orders/GetOrderTests.cs
@@ -0,0 +1,55 @@
+﻿using Application.Handlers.Order;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Test.Core.Fixtures;
+using Test.Core.Helpers;
+using Xunit;
+using static Application.Contracts.Order.Queries.GetOrder;
+
+namespace Test.Handlers.Orders;
+
+[Collection(nameof(CoreDatabaseCollectionFixture))]
+public class GetOrderTests : IAsyncLifetime, IClassFixture<CoreDatabaseFixture>
+{
+    private readonly CoreDatabaseFixture _database;
+
+    public GetOrderTests(CoreDatabaseFixture database)
+    {
+        _database = database;
+    }
+    
+    [Fact]
+    public async Task Handle_Should_NotFind()
+    {
+        var query = new Query(Guid.NewGuid());
+        var handler = new GetOrderHandler(_database.Context);
+
+        var response = await handler.Handle(query, CancellationToken.None);
+
+        response.IsSuccess.Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task Handle_Should_NotThrow()
+    {
+        await SeedingHelper.SeedDatabaseAsync(_database.Context);
+
+        var order = await _database.Context.Orders.FirstAsync();
+        var query = new Query(order.OrderId);
+        var handler = new GetOrderHandler(_database.Context);
+
+        var response = await handler.Handle(query, CancellationToken.None);
+
+        response.IsSuccess.Should().BeTrue();
+    }
+
+    public Task InitializeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task DisposeAsync()
+    {
+        return _database.ResetAsync();
+    }
+}
