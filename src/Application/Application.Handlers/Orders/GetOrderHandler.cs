84370774c92d897772b28c0d5bfc50077b788738
xvsvg <pobedotriples@gmail.com>
2023-06-23 18:23:11 +0300

    fix: naming

--- src/Application/Application.Handlers/Orders/GetOrderHandler.cs
+++ src/Application/Application.Handlers/Orders/GetOrderHandler.cs
@@ -1,12 +1,13 @@
 ﻿using Application.Common.Exceptions;
 using Application.DataAccess.Contracts;
+using Domain.Core.Implementations;
 using Infrastructure.Mapping.Orders;
 using LanguageExt.Common;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using static Application.Contracts.Order.Queries.GetOrder;
 
-namespace Application.Handlers.Order;
+namespace Application.Handlers.Orders;
 
 internal class GetOrderHandler : IRequestHandler<Query, Result<Response>>
 {
@@ -19,17 +20,16 @@ internal class GetOrderHandler : IRequestHandler<Query, Result<Response>>
 
     public async Task<Result<Response>> Handle(Query request, CancellationToken cancellationToken)
     {
-        var order  = await _context.Orders
+        var order = await _context.Orders
             .FirstOrDefaultAsync(x => x.OrderId.Equals(request.Id), cancellationToken);
 
         if (order is null)
         {
-            var error = EntityNotFoundException.For<Domain.Core.Implementations.Order>(request.Id);
+            var error = EntityNotFoundException.For<Order>(request.Id);
             return new Result<Response>(error);
         }
 
         var response = new Response(order.ToDto());
-        
-        return new Result<Response>(response);
+        return response;
     }
 }
