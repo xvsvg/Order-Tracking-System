d257146ae13b9573c0a553c8937f3fe9b709ac02
xvsvg <pobedotriples@gmail.com>
2023-06-23 18:38:33 +0300

    feat: add cache configuration

---
+++ src/Presentation/Presentation.Endpoints/Tools/CacheConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace Presentation.Endpoints.Tools;
+
+public class CacheConfiguration
+{
+    public CacheConfiguration(int time)
+        => TimeInSeconds = time;
+    
+    public int TimeInSeconds { get; init; }
+}
