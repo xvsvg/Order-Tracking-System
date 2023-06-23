65c3fcdcc443c067159c7ffe6e94f14189be2bdb
xvsvg <pobedotriples@gmail.com>
2023-06-23 18:22:56 +0300

    feat: add handler registration extensions

---
+++ src/Application/Application.Handlers/Extensions/RegistrationExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Application.Contracts.Tools;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Application.Handlers.Extensions;
+
+public static class RegistrationExtensions
+{
+    private const int DefaultPageItemCount = 1;
+
+    public static IServiceCollection AddHandlers(this IServiceCollection collection, IConfiguration configuration)
+    {
+        var paginationConfiguration = configuration.GetSection("Pagination").Get<PaginationConfiguration>()
+                                      ?? new PaginationConfiguration(DefaultPageItemCount);
+
+        collection.TryAddSingleton(paginationConfiguration);
+
+        collection.AddMediatR(c => c.RegisterServicesFromAssemblyContaining<IAssemblyMarker>());
+        return collection;
+    }
+}
