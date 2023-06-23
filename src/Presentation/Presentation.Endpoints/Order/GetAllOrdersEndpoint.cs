1be87afe3b50bcf00d06cbd0e7bfef6c728384a2
xvsvg <pobedotriples@gmail.com>
2023-06-23 18:39:12 +0300

    feat: add get all orders endpoint

---
+++ src/Presentation/Presentation.Endpoints/Order/GetAllOrdersEndpoint.cs
@@ -0,0 +1,42 @@
+﻿using FastEndpoints;
+using FluentValidation;
+using MediatR;
+using Presentation.Endpoints.Tools;
+using static Application.Contracts.Order.Queries.GetAllOrders;
+
+namespace Presentation.Endpoints.Order;
+
+internal class GetAllOrdersEndpoint : Endpoint<Query, Response>
+{
+    private readonly CacheConfiguration _cacheConfiguration;
+    private readonly IMediator _mediator;
+
+    public GetAllOrdersEndpoint(CacheConfiguration cacheConfiguration, IMediator mediator)
+    {
+        _cacheConfiguration = cacheConfiguration;
+        _mediator = mediator;
+    }
+
+    public override void Configure()
+    {
+        ResponseCache(_cacheConfiguration.TimeInSeconds, varyByQueryKeys: new[] { "page" });
+        Verbs(Http.GET);
+        Routes("api/orders");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(Query req, CancellationToken ct)
+    {
+        var response = await _mediator.Send(req, ct);
+
+        await response.Match(
+            o => SendOkAsync(o, cancellation: ct),
+            err =>
+            {
+                if (err is ValidationException ex)
+                    return SendErrorsAsync(cancellation: ct);
+                        
+                return SendNoContentAsync(cancellation: ct);
+            });
+    }
+}
