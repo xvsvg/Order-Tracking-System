2438aaa26fda57167d9ba64f1ecb1dd00f37810a
xvsvg <pobedotriples@gmail.com>
2023-06-23 18:38:22 +0300

    feat: modify get order endpoint

--- src/Presentation/Presentation.Endpoints/Order/GetOrderEndpoint.cs
+++ src/Presentation/Presentation.Endpoints/Order/GetOrderEndpoint.cs
@@ -1,27 +1,36 @@
 ﻿using FastEndpoints;
 using MediatR;
-using Microsoft.AspNetCore.Authorization;
+using Presentation.Endpoints.Tools;
 using static Application.Contracts.Order.Queries.GetOrder;
 
 namespace Presentation.Endpoints.Order;
 
-[HttpGet("api/orders", "api/orders/{id:guid}")]
-[AllowAnonymous]
 internal class GetOrderEndpoint : Endpoint<Query, Response>
 {
     private readonly IMediator _mediator;
+    private readonly CacheConfiguration _cacheConfiguration;
 
-    public GetOrderEndpoint(IMediator mediator)
+    public GetOrderEndpoint(IMediator mediator, CacheConfiguration cacheConfiguration)
     {
         _mediator = mediator;
+        _cacheConfiguration = cacheConfiguration;
+    }
+
+    public override void Configure()
+    {
+        ResponseCache(_cacheConfiguration.TimeInSeconds);
+        Verbs(Http.GET);
+        Routes("api/orders/{id:guid}");
+        AllowAnonymous();
     }
 
     public override async Task HandleAsync(Query req, CancellationToken ct)
     {
         var response = await _mediator.Send(req, ct);
 
-        await response.Match(
-            o => SendOkAsync(o, cancellation: ct),
-            err => SendNotFoundAsync(cancellation: ct));
+        if (response.Order is null)
+            await SendNotFoundAsync(ct);
+
+        await SendOkAsync(response, ct);
     }
 }
