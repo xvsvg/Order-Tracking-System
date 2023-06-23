817cb32de0c41544f72d2090d298a5738a7ba367
xvsvg <pobedotriples@gmail.com>
2023-06-23 18:19:07 +0300

    feat: add playground.web

---
+++ src/Playground/Playground.Web/Program.cs
@@ -0,0 +1,23 @@
+using Application.Handlers.Extensions;
+using Application.Validation.Extensions;
+using Application.Validation.Middleware;
+using Infrastructure.DataAccess.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Presentation.Endpoints.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDatabaseContext(o => o
+    .UseNpgsql("Host=localhost;Port=1433;Database=ots;Username=postgres;Password=postgres")
+    .UseLazyLoadingProxies());
+    
+builder.Services.AddHandlers(builder.Configuration);
+builder.Services.AddEndpoints(builder.Configuration);
+builder.Services.AddValidation();
+builder.Services.AddTransient<ValidationMappingMiddleware>();
+
+var app = builder.Build();
+
+app.UseMiddleware<ValidationMappingMiddleware>();
+app.UseEndpoints();
+app.Run();
