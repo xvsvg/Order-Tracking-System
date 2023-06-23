e6b6f4f8915e66ad522648ec7e58b027417ccb18
xvsvg <pobedotriples@gmail.com>
2023-06-23 18:26:08 +0300

    feat: add pagination configuration

---
+++ src/Application/Application.Contracts/Tools/PaginationConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Contracts.Tools;
+
+public class PaginationConfiguration
+{
+    public PaginationConfiguration(int size)
+        => PageSize = size;
+    
+    public int PageSize { get; init; }
+}
