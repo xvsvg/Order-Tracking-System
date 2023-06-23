327350c19650a2dccb30b0d18ada911fffa9fa83
xvsvg <pobedotriples@gmail.com>
2023-06-23 18:25:51 +0300

    feat: add IValidatableRequest

---
+++ src/Application/Application.Contracts/Validation/IValidatableRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Application.Contracts.Validation;
+
+public interface IValidatableRequest<out TResponse> : IRequest<TResponse>, IValidatableRequest
+{
+}
+
+public interface IValidatableRequest
+{
+}
