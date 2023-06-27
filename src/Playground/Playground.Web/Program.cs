a8c98c4de579cb55a0d7a57d6647668a59c9548a
xvsvg <pobedotriples@gmail.com>
2023-06-27 18:59:19 +0300

    feat: update usings

--- src/Playground/Playground.Web/Program.cs
+++ src/Playground/Playground.Web/Program.cs
@@ -5,19 +5,27 @@ using Infrastructure.DataAccess.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Presentation.Endpoints.Extensions;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace Playground.Web;
 
-builder.Services.AddDatabaseContext(o => o
-    .UseNpgsql("Host=localhost;Port=1433;Database=ots;Username=postgres;Password=postgres")
-    .UseLazyLoadingProxies());
+public partial class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddDatabaseContext(o => o
+            .UseNpgsql("Host=localhost;Port=1433;Database=ots;Username=postgres;Password=postgres")
+            .UseLazyLoadingProxies());
     
-builder.Services.AddHandlers(builder.Configuration);
-builder.Services.AddEndpoints(builder.Configuration);
-builder.Services.AddValidation();
-builder.Services.AddTransient<ValidationMappingMiddleware>();
+        builder.Services.AddHandlers(builder.Configuration);
+        builder.Services.AddEndpoints(builder.Configuration);
+        builder.Services.AddValidation();
+        builder.Services.AddTransient<ValidationMappingMiddleware>();
 
-var app = builder.Build();
+        var app = builder.Build();
 
-app.UseMiddleware<ValidationMappingMiddleware>();
-app.UseEndpoints();
-app.Run();
+        app.UseMiddleware<ValidationMappingMiddleware>();
+        app.UseEndpoints();
+        app.Run();
+    }
+}
