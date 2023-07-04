9677b56f699317cb52ee62263132b1179d36d866
xvsvg <pobedotriples@gmail.com>
2023-07-04 18:21:23 +0300

    feat: add delete customer feature

---
+++ src/Presentation/Presentation.Endpoints/Customers/DeleteCustomerEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using FastEndpoints;
+using MediatR;
+using static Application.Contracts.Customer.Commands.DeleteCustomer;
+
+namespace Presentation.Endpoints.Customers;
+
+internal class DeleteCustomerEndpoint : Endpoint<Command>
+{
+    private readonly IMediator _mediator;
+
+    public DeleteCustomerEndpoint(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public override void Configure()
+    {
+        Verbs(Http.DELETE);
+        Routes("api/customers");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(Command req, CancellationToken ct)
+    {
+        var response = await _mediator.Send(req, ct);
+
+        await response.Match(
+            x => SendNoContentAsync(ct),
+            err => SendErrorsAsync(cancellation: ct));
+    }
+}
