efd6c576efba8aa723bda251aac0542100bcaa98
xvsvg <pobedotriples@gmail.com>
2023-07-04 18:21:23 +0300

    feat: add delete customer feature

---
+++ tests/ApplicationMicroservice/Test.Core/Handlers/Customers/DeleteCustomerTests.cs
@@ -0,0 +1,48 @@
+﻿using Application.Handlers.Customers;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Test.Core.Fixtures;
+using Test.Tools.Helpers;
+using Xunit;
+using static Application.Contracts.Customer.Commands.DeleteCustomer;
+
+namespace Test.Core.Handlers.Customers;
+
+[Collection(nameof(CoreDatabaseCollectionFixture))]
+public class DeleteCustomerTests : IAsyncLifetime
+{
+    private readonly CoreDatabaseFixture _database;
+    private readonly DeleteCustomerHandler _handler;
+
+    public DeleteCustomerTests(CoreDatabaseFixture database)
+    {
+        _database = database;
+        _handler = new DeleteCustomerHandler(_database.Context);
+    }
+
+    [Fact]
+    public async Task DeleteCustomer_ShouldNotThrow()
+    {
+        await SeedingHelper.SeedDatabaseAsync(_database.Context);
+        var customer = await _database.Context.Customers.FirstAsync();
+        var command = new Command(customer.PersonId);
+        
+        var response = await _handler.Handle(command, default);
+
+        var result = response.Match(
+            x => customer,
+            err => null!);
+
+        result.Should().NotBeNull();
+    }
+
+    public Task InitializeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task DisposeAsync()
+    {
+        return _database.ResetAsync();
+    }
+}
---
+++ tests/ApplicationMicroservice/Test.Endpoints/Customers/DeleteCustomerEndpointTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using Application.DataAccess.Contracts;
+using FastEndpoints;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Presentation.Endpoints.Customers;
+using Test.Endpoints.Fixtures;
+using Xunit;
+using static Application.Contracts.Customer.Commands.DeleteCustomer;
+
+namespace Test.Endpoints.Customers;
+
+[Collection(nameof(WebFactoryCollection))]
+public class DeleteCustomerEndpointTests : IAsyncLifetime
+{
+    private readonly WebFactory _factory;
+    private readonly HttpClient _client;
+    private readonly IDatabaseContext _context;
+
+    public DeleteCustomerEndpointTests(WebFactory factory)
+    {
+        _factory = factory;
+        _client = factory.CreateClient();
+        _context = factory.Context;
+    }
+
+    [Fact]
+    public async Task DeleteCustomer_ShouldNotThrow()
+    {
+        var customer = await _context.Customers.FirstAsync();
+        
+        var command = new Command(customer.PersonId);
+
+        var response = await _client
+            .DELETEAsync<DeleteCustomerEndpoint, Command>(command);
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+
+    public Task InitializeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task DisposeAsync()
+    {
+        return _factory.ResetAsync();
+    }
+}
