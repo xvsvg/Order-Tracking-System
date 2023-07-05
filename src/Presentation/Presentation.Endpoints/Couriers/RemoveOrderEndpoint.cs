925524e72587d8b5ef4d2223259a6eb48e58a42f
xvsvg <pobedotriples@gmail.com>
2023-07-05 18:13:40 +0300

    feat: add remove order from delivery list feature

---
+++ src/Presentation/Presentation.Endpoints/Couriers/RemoveOrderEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using FastEndpoints;
+using MediatR;
+using static Application.Contracts.Courier.Commands.RemoveOrder;
+
+namespace Presentation.Endpoints.Couriers;
+
+internal class RemoveOrderEndpoint : Endpoint<Command>
+{
+    private readonly IMediator _mediator;
+
+    public RemoveOrderEndpoint(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public override void Configure()
+    {
+        Verbs(Http.DELETE);
+        Routes("api/couriers/{customerId:guid}/orders/{orderId:guid}");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(Command req, CancellationToken ct)
+    {
+        var response = await _mediator.Send(req, ct);
+
+        await response.Match(
+            x => SendNoContentAsync(ct),
+            err => SendErrorsAsync(cancellation: ct));
+    }
+}
