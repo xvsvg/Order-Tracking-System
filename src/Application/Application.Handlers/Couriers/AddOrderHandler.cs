c4f5fffde6815f36c21a3f8c469dd98894da3d7c
xvsvg <pobedotriples@gmail.com>
2023-07-05 18:20:12 +0300

    feat: add add order for delivery feature

---
+++ src/Application/Application.Handlers/Couriers/AddOrderHandler.cs
@@ -0,0 +1,45 @@
+﻿using Application.Common.Exceptions;
+using Application.DataAccess.Contracts;
+using Domain.Core.Implementations;
+using LanguageExt.Common;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using static Application.Contracts.Courier.Commands.AddOrder;
+
+namespace Application.Handlers.Couriers;
+
+internal class AddOrderHandler : IRequestHandler<Command, Result<Unit>>
+{
+    private readonly IDatabaseContext _context;
+
+    public AddOrderHandler(IDatabaseContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+    {
+        var courier = await _context.Couriers
+            .FirstOrDefaultAsync(x => x.PersonId.Equals(request.CourierId), cancellationToken);
+
+        var order = await _context.Orders
+            .FirstOrDefaultAsync(x => x.OrderId.Equals(request.OrderId), cancellationToken);
+
+        if (order is null)
+        {
+            var error = EntityNotFoundException.For<Order>(request.OrderId);
+            return new Result<Unit>(error);
+        }
+        
+        if (courier is null)
+        {
+            var error = EntityNotFoundException.For<Courier>(request.OrderId);
+            return new Result<Unit>(error);
+        }
+        
+        courier.RemoveDeliveredOrder(order);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
---
+++ src/Presentation/Presentation.Endpoints/Couriers/AddOrderEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using FastEndpoints;
+using MediatR;
+using static Application.Contracts.Courier.Commands.AddOrder;
+
+namespace Presentation.Endpoints.Couriers;
+
+internal class AddOrderEndpoint : Endpoint<Command>
+{
+    private readonly IMediator _mediator;
+
+    public AddOrderEndpoint(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public override void Configure()
+    {
+        Verbs(Http.PUT);
+        Routes("api/couriers/{courierId:guid}/orders/{orderId:guid}");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(Command req, CancellationToken ct)
+    {
+        var response = await _mediator.Send(req, ct);
+
+        await response.Match(
+            x => SendNoContentAsync(ct),
+            err => SendErrorsAsync(cancellation: ct));
+    }
+}
