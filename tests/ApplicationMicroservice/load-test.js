7d5b65935d09d7f2dd47f9d6a6eb8b290b786fc9
xvsvg <pobedotriples@gmail.com>
2023-07-06 14:52:41 +0300

    feat: add load test

---
+++ tests/ApplicationMicroservice/load-test.js
@@ -0,0 +1,22 @@
+﻿import http from 'k6/http'
+import {sleep} from 'k6'
+
+export let options = {
+    insecureSkipTLSVerify: true,
+    noConnectionReuse: false,
+    stages: [
+        {duration: '10s', target: 1000},
+        {duration: '20s', target: 1000},
+        {duration: '10s', target: 0}
+    ],
+    thresholds: {
+        http_req_duration: ['p(99) < 150'],
+    },
+};
+
+export default () => {
+    for (let i = 0; i < 10; ++i)
+        http.get(`http://localhost:5163/api/orders?page=${i}`)
+
+    sleep(1)
+}
