19136f413b5278170368ecda29bcf3685f8e74ff
xvsvg <pobedotriples@gmail.com>
2023-07-09 19:49:20 +0300

    feat: vault

--- Presentation.WebAPI/Program.cs
+++ Presentation.WebAPI/Program.cs
@@ -3,9 +3,10 @@ using Presentation.WebAPI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var webApiConfiguration = new WebApiConfiguration(builder.Configuration);
+var webApiConfiguration = new WebApiConfiguration(builder.Configuration, builder.Environment);
+builder.Configuration.AddSecrets(webApiConfiguration);
 
-builder.Services.ConfigureServices(builder.Configuration, webApiConfiguration);
+builder.Services.ConfigureServices(builder.Configuration);
 
 var app = builder.Build().ConfigureApplication();
 
