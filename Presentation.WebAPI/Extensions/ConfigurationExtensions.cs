34f9cd3bbb82db7f64a65dec221d8762a4d1e00d
xvsvg <pobedotriples@gmail.com>
2023-07-09 19:48:21 +0300

    feat: add configuration extensions

---
+++ Presentation.WebAPI/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Presentation.WebAPI.Configuration;
+using VaultSharp.Extensions.Configuration;
+
+namespace Presentation.WebAPI.Extensions;
+
+public static class ConfigurationExtensions
+{
+    public static IConfigurationBuilder AddSecrets(this IConfigurationBuilder builder,
+        WebApiConfiguration configuration)
+    {
+        var uri = configuration.ASPNETCORE_ENVIRONMENT is "Development"
+            ? "http://localhost:8200"
+            : "http://vault:8200";
+
+        var cgf = builder
+            .AddVaultConfiguration(() => new VaultOptions(
+                uri,
+                configuration.VaultToken),
+            $"{typeof(ConfigurationExtensions).Assembly.GetName().Name}/{configuration.ASPNETCORE_ENVIRONMENT}",
+            "ots");
+
+        return builder;
+    }
+}
