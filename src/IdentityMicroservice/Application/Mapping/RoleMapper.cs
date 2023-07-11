24c35a9fcddaa147f1258fb4846f2804662d529d
xvsvg <pobedotriples@gmail.com>
2023-07-11 19:16:44 +0300

    feat: add mapping

---
+++ src/IdentityMicroservice/Application/Mapping/RoleMapper.cs
@@ -0,0 +1,13 @@
+﻿using Application.Dto;
+using Application.Exceptions;
+using DataAccess.Models;
+
+namespace Application.Mapping;
+
+public static class RoleMapper
+{
+    public static RoleDto ToDto(this OtsIdentityRole role)
+    {
+        return new RoleDto(role.Id, role.Name ?? throw new UnknownRoleException());
+    }
+}
---
+++ src/IdentityMicroservice/Application/Mapping/UserMapper.cs
@@ -0,0 +1,13 @@
+﻿using Application.Dto;
+using Application.Exceptions;
+using DataAccess.Models;
+
+namespace Application.Mapping;
+
+public static class UserMapper
+{
+    public static UserDto ToDto(this OtsIdentityUser user)
+    {
+        return new UserDto(user.Id, user.UserName ?? throw new UnknownUsernameException());
+    }
+}
