8dea9a7d10686b3ddddc0be9a32f187163792187
xvsvg <pobedotriples@gmail.com>
2023-07-11 19:16:18 +0300

    feat: add identity context

---
+++ src/IdentityMicroservice/DataAccess/IdentityContext.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess;
+
+public sealed class IdentityContext : IdentityDbContext<OtsIdentityUser, OtsIdentityRole, Guid>
+{
+    public IdentityContext(DbContextOptions<IdentityContext> options)
+        : base(options)
+    {
+        Database.EnsureCreated();
+    }
+}
