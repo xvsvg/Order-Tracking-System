baabcd8b6f8e71acdc125113453b0d7f04c39bda
xvsvg <pobedotriples@gmail.com>
2023-07-11 19:15:45 +0300

    feat: add data access extensions

---
+++ src/IdentityMicroservice/DataAccess/Extensions/IdentityResultExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DataAccess.Extensions;
+
+public static class IdentityResultExtensions
+{
+    public static void EnsureSucceeded(this IdentityResult result)
+    {
+        if (result.Succeeded is false)
+            throw new ApplicationException(
+                string.Join(' ',
+                    result.Errors.Select(x => x.Description)));
+    }
+}
---
+++ src/IdentityMicroservice/DataAccess/Extensions/UserManagerExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Application.Common.Exceptions;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace DataAccess.Extensions;
+
+public static class UserManagerExtensions
+{
+    public static async Task<OtsIdentityUser> GetByNameAsync(
+        this UserManager<OtsIdentityUser> manager,
+        string username,
+        CancellationToken cancellationToken)
+    {
+        var user = await manager.FindByNameAsync(username);
+
+        if (user is null)
+            throw EntityNotFoundException.For<OtsIdentityUser>(username);
+
+        return user;
+    }
+}
