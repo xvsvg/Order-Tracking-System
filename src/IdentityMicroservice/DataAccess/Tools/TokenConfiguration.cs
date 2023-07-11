cff29064ce9d5316d6b3cf6c170286db79fff63e
xvsvg <pobedotriples@gmail.com>
2023-07-11 19:16:01 +0300

    feat: add token config

---
+++ src/IdentityMicroservice/DataAccess/Tools/TokenConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace DataAccess.Tools;
+
+public class TokenConfiguration
+{
+    public string Secret { get; set; } = default!;
+    public string Issuer { get; set; } = default!;
+    public string Audience { get; set; } = default!;
+    public TimeSpan Expiration { get; set; } = default!;
+}
